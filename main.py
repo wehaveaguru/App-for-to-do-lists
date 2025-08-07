0dc6c5d799dee06bd54c2ade45aa1be7b5ac63f9
Anbeeswaran <r.anbeeswaran@gmail.com>
2025-08-07 19:59:46 +0530

    File Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import Tk,Label,Entry,Button,font,END
 
 window=Tk()
 window.title("To-Do List")
@@ -11,15 +11,26 @@ title_label.grid(row=0, column=1)
 task_entry=Entry(width=30)
 task_entry.grid(row=1, column=0, columnspan=2)
 task_entry.focus()
+def mark_task_done(task_label):
+    font_style=font.Font(family=task_label,overstrike=True)
+    task_label.config(font=font_style)
 
+def remove_task(task_label, check_button, remove_button):
+    task_label.destroy()
+    check_button.destroy()
+    remove_button.destroy()
+    task_entry.delete(0, END)
 def add_task():
     task=task_entry.get()
-    if task:
-        with open("tasks.txt","a") as f:
-            f.write(f"{task}\n")
-        task_entry.delete(0, END)
+    task_label=Label(text=task)
+    task_label.grid(row=2, column=0)
+    check_button=Button(text="✓", command=lambda: mark_task_done(task_label))
+    check_button.grid(row=2, column=1)
+    remove_button=Button(text="X", command=lambda: remove_task(task_label,check_button,remove_button))
+    remove_button.grid(row=2, column=2)
 add_task_button=Button(text="Add Task", width=15, command=add_task)
 add_task_button.grid(row=1,column=2)
+
 
 window.mainloop()
 
